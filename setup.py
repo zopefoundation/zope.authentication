e7dc41b4c101fcb52038cbfb2b65035b2346ad0d
Tres Seaver <tseaver@palladion.com>
2010-05-05 16:59:40 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006-2009 Zope Corporation and Contributors.
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(name='zope.authentication',
       version = '3.7.2dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Definition of authentication basics for the Zope Framework',
       long_description=(
