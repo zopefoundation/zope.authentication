6d9c5e870937a57d48235a196fbdb0d527667cf1
Stephan Richter <stephan.richter@gmail.com>
2013-02-21 16:43:03 +0000

    Preparing release 4.1.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.authentication',
-      version = '4.0.1dev',
+      version='4.1.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Definition of authentication basics for the Zope Framework',
