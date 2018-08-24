e4c560b3d3c8fee76e719c43c96408221ca41adb
Jason Madden <jamadden@gmail.com>
2018-08-24 10:51:28 -0500

    Back to development: 4.4.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.authentication',
-      version='4.4.0',
+      version='4.4.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Definition of authentication basics for the Zope Framework',
