0750d178b4e44141968702f02ff9a2a63d55cb40
Jason Madden <jamadden@gmail.com>
2021-03-19 06:30:48 -0500

    Preparing release 4.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.authentication',
-      version='4.5.0.dev0',
+      version='4.5.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Definition of authentication basics for the Zope Framework',
