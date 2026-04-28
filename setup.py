30d842b78a1437acf3415207cdf753ba1b084f8a
Michael Howitz <icemac@gmx.net>
2026-04-28 08:52:51 +0200

    Refactor package setup to use pyproject.toml for metadata and dependencies

--- setup.py
+++ setup.py
@@ -13,70 +13,8 @@
 ##############################################################################
 """Setup for zope.authentication package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-]
-
-setup(name='zope.authentication',
-      version='6.1.dev0',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      description='Definition of authentication basics for the Zope Framework',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')
-      ),
-      keywords="zope security authentication",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: 3.14',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope :: 3',
-      ],
-      url='https://zopeauthentication.readthedocs.io/',
-      license='ZPL-2.1',
-      extras_require={
-          'test': TESTS_REQUIRE,
-          'docs': [
-              'Sphinx',
-              'repoze.sphinx.autointerface',
-          ],
-      },
-      python_requires='>=3.10',
-      install_requires=[
-          'setuptools',
-          'zope.browser',
-          'zope.component>=3.6.0',
-          'zope.i18nmessageid',
-          'zope.interface >= 3.8',
-          'zope.schema',
-          'zope.security',
-      ],
-      include_package_data=True,
-      zip_safe=False,
-      )
+# See pyproject.toml for package metadata
+setup()
